97da401a020fb6edc7d24857b435e3291cf6d302
mariodedapolito <s266280@studenti.polito.it>
2021-11-05 22:25:02 +0100

    rename SGP to SPG

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SGP - Solidarity Purchasing Group
+          SPG - Solidarity Purchasing Group
         </p>
       </header>
     </div>
