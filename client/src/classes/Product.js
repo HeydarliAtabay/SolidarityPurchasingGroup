5da0c9aebacc14fc2c7b0500a0ca08a68ae61be4
mariodedapolito <s266280@studenti.polito.it>
2021-11-11 16:37:47 +0100

    add more tables to db + dummy data + Products class

--- client/src/classes/Product.js
+++ client/src/classes/Product.js
@@ -1,16 +1,20 @@
 
 class Product{
-    constructor(id, productName, productPrice, productWeight, productProvider){
+    constructor(id, productName, productDescription, productCategory, productPrice, productUnit, productQuantity, productExpiryDate, productProviderId, productProviderName){
         this.id = id;
         this.name = productName;
+        this.description = productDescription;
+        this.category = productCategory;
         this.price = productPrice;
-        this.weight = productWeight;
-        this.provider = productProvider;
-
+        this.unit = productUnit;
+        this.quantity = productQuantity;
+        this.expiryDate = productExpiryDate; 
+        this.providerId = productProviderId;
+        this.providerName = productProviderName;
     }
 
     static from(json){
-        return new Product(json.id,json.productName,json.productPrice,json.productWeight,json.productProvider);
+        return new Product(json.id,json.productName,json.productDescription,json.productCategory,json.productPrice,json.productUnit,json.productQuantity,json.productExpiryDate,json.productProviderId,json.productProviderName);
     }
 }
 
