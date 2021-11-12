4886a6de205909d3d40b87babab35dd8804e504b
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-11-12 02:39:03 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@
 
 const express = require("express");
 const morgan = require("morgan"); // logging middleware
-const passportLocal = require("passport-local").Strategy; //Authentication strategy
+const clientsDao = require('./DAOs/clients-dao');
+const ordersDao = require('./DAOs/client-orders-dao');
+//const passportLocal = require("passport-local").Strategy; //Authentication strategy
 const session = require("express-session"); //Session middleware
-const passport = require("passport"); //Authentication middleware
+//const passport = require("passport"); //Authentication middleware
 
 // init express
 const app = express();
@@ -13,3 +15,43 @@ const PORT = 3001;
 // set-up the middlewares
 app.use(morgan("dev"));
 app.use(express.json());
+
+
+
+//GET all clients and budget
+app.get('/api/clients', async (req, res) => {
+    try {
+        const o = await clientsDao.getAllClients();
+        return res.status(200).json(o);
+    } catch (err) {
+        res.status(500).json({ code: 500, error: `Database error during the retrieve of the list of clients.` });
+    }
+});
+
+//GET all client orders
+app.get('/api/orders', async (req, res) => {
+    try {
+        const m = await ordersDao.getAllOrders();
+        return res.status(200).json(m);
+    } catch (err) {
+        res.status(500).json({ code: 500, error: "Database error during the retrieve of the list of orders." });
+    }
+});
+
+//PUT to update a product as delivered
+    app.put('/api/orders/:order_id',
+    
+        async (req, res) => {
+           
+            try {
+                await ordersDao.delivered(req.params.order_id);
+                res.status(200).end("Update Completed!");
+            } catch (err) {
+                res.status(503).json({ code: 503, error: `Unavailable service during the update of order` });
+            }
+    
+        });
+
+
+/* CONNECTION */
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
