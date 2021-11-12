43379eab724310c79d5e5b13a36efa18c6e8ecba
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-11-12 02:41:21 +0100

    Update clients-dao.js

--- server/DAOs/clients-dao.js
+++ server/DAOs/clients-dao.js
@@ -7,3 +7,20 @@ const db = new sqlite.Database('spg.db', (err) => {
         throw err;
     }
 });
+
+//get-> retrieve clients and their budget 
+exports.getAllClients=()=>{
+    return new Promise((resolve,reject)=>
+    {const sql='SELECT * from clients';
+   
+    db.all(sql,[], (err,rows)=>{
+        if (err){
+            reject(err);
+            return;
+        }
+        const o=rows.map((e)=>({client_id:e.client_id, budget :e.budget}));
+        resolve(o);
+    });
+
+}   );
+};
