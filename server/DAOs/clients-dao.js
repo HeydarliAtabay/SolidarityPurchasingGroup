ba28a8ed2996b4408ff62a9936c01a4a16d13c97
Atabay Heydarli <62261953+HeydarliAtabay@users.noreply.github.com>
2021-11-13 22:14:30 +0100

    CreateClient DAO was added

--- server/DAOs/clients-dao.js
+++ server/DAOs/clients-dao.js
@@ -24,3 +24,21 @@ exports.getAllClients=()=>{
 
 }   );
 };
+
+/// Adding new client to the db //////
+
+exports.createClient=(client)=>{
+    return new Promise((resolve, reject)=>{
+      const sql = 'INSERT INTO clients(budget,name,surname, gender, birthdate, country, region, address, city, phone,email, hash) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)'
+      db.run(sql, [client.budget,client.name, client.surname, client.gender, client.birthdate, client.country, client.region,
+                    client.address, client.city, client.phone, client.email, client.hash
+    ], function(err){
+        if(err){
+          reject(err);
+          return;
+        }
+        console.log(this.lastID);
+        resolve(this.lastID)
+      });
+    });
+  };
