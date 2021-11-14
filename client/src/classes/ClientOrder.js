b67e39c6003b415ecfcf3563ac6275d2aa9f5600
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-11-14 17:30:16 +0100

    orders class

---
+++ client/src/classes/ClientOrder.js
@@ -0,0 +1,8 @@
+
+function clientOrders(order_id,client_id,product_name,state){
+    this.order_id=order_id; 
+    this.client_id=client_id; 
+    this.product_name=product_name;
+    this.state=state;
+    }
+export  {clientOrders};
