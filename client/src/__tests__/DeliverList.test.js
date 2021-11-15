c65505fa7ee0b82942a433fe1d3135d816036cdc
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-11-15 19:12:05 +0100

    Add files via upload

---
+++ client/src/__tests__/DeliverList.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {  render } from '@testing-library/react';
+import  DeliverList from '../Components/DeliverList'
+
+
+const testOrders = [{ order_d: 1, client_id: 1, product_name: "fennels",state:"booked" }];
+//test('Renders button to show products Correctly', () => {
+//    const renderResult=render(<DeliverList orders={testOrders}  />);
+//    expect(renderResult.getByText("Show products to be delivered")).toBeInTheDocument();
+   
+//});
+test('Renders list of products to deliver Correctly', () => {
+    const renderResult=render(<DeliverList orders={testOrders} b={"booked"} />);
+    expect(renderResult.getByText(1)).toBeInTheDocument();
+    expect(renderResult.getByText(1)).toBeInTheDocument();
+    expect(renderResult.getByText("fennels")).toBeInTheDocument();
+    
+});
