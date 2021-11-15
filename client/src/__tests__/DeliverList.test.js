3a4928cf14a9d1b38bd86c6162e36274bf11e5e5
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-11-15 19:19:39 +0100

    Update DeliverList.test.js

--- client/src/__tests__/DeliverList.test.js
+++ client/src/__tests__/DeliverList.test.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import {  render } from '@testing-library/react';
 import  DeliverList from '../Components/DeliverList'
+import  EmployeePage from '../Components/EmployeePage'
 
 
 const testOrders = [{ order_d: 1, client_id: 1, product_name: "fennels",state:"booked" }];
-//test('Renders button to show products Correctly', () => {
-//    const renderResult=render(<DeliverList orders={testOrders}  />);
-//    expect(renderResult.getByText("Show products to be delivered")).toBeInTheDocument();
+    test('Renders button to show products Correctly', () => {
+    const renderResult=render(<EmployeePage orders={testOrders}  />);
+    expect(renderResult.getByText("Show products to be delivered")).toBeInTheDocument();
    
-//});
+});
 test('Renders list of products to deliver Correctly', () => {
     const renderResult=render(<DeliverList orders={testOrders} b={"booked"} />);
     expect(renderResult.getByText(1)).toBeInTheDocument();
