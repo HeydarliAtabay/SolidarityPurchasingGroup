fb98e8827b5c75dbf7da22eeb61a4e731f849d2c
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-11-15 19:44:34 +0100

    Add files via upload

---
+++ client/src/Components/DeliverList.js
@@ -0,0 +1,47 @@
+import { Container, Button, Row, Col, ListGroup, ListGroupItem, Image, Modal, Form } from 'react-bootstrap';
+import ris from './reply-all-fill.svg';
+function DeliverList(props){
+    return(
+    <ListGroup variant="flush">
+    <ListGroupItem key={"hi*"} style={{'backgroundColor':"#ffb6c1"}}>
+        <Row>
+    <Col xs={3} md={3}>ORDER_ID</Col>
+    <Col xs={3} md={3}>CLIENT_ID</Col>
+    <Col xs={3} md={3}>PRODUCT</Col>
+    <Col xs={3} md={3}>DELIVER</Col>
+    </Row></ListGroupItem>
+    {props.orders.filter(x=>x.state===props.b).map((s)=>
+    
+    <ListGroupItem key={s.order_id} style={{'backgroundColor':"#ffe4e1"}}>
+      <Row><Col xs={3} md={3}>{s.order_id}</Col>
+    <Col xs={3} md={3}>{s.client_id}</Col>
+    <Col xs={3} md={3}>{s.product_name}</Col>
+    
+    
+    <Col xs={3} md={3}> 
+    <Image src={ris}data-testid="im" style={{'cursor':'pointer'}} onClick={()=>{
+       API.updateDelivered(s.order_id).then(()=>{
+           
+       
+              props.setRecharged(true);
+              
+         })
+         //api->update state
+    //set recharged della tabella ordini-clienti
+    }}></Image>
+    </Col>
+    </Row>
+    </ListGroupItem>)
+    
+    }<ListGroupItem>
+    <Button variant={"light"}style={{'borderStyle':'hidden','backgroundColor':"#ffb6c1",'position':'absolute' , 'right':'15px'}}onClick={()=>{props.setShow(false);}}>Close</Button></ListGroupItem>
+    </ListGroup>
+    
+    
+    );}
+    
+    
+    
+    
+    
+    export default DeliverList;
