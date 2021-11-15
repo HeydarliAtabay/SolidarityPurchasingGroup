50cae6f34cf968136887a9e2e8dff0a2239a5ffa
mariodedapolito <69730101+mariodedapolito@users.noreply.github.com>
2021-11-15 16:15:05 +0100

    add API tests

--- client/src/__tests__/API.test.js
+++ client/src/__tests__/API.test.js
@@ -77,3 +77,170 @@ describe('test updateDelivered(order_id)', () => {
    
 });
 });
+
+/*______________________________________________________________________*/
+/*                      PRODUCTS API TESTING                            */
+/*______________________________________________________________________*/
+
+const products = [
+    { id: 1, name: 'Carrots', description: 'Some description 1', category: 'Vegetables', price: 1.23, unit: 'kg', quantity: 110, expiryDate: '2021-11-20', providerId: 1, providerName: 'Luca Bianchi', active: 1 },
+    { id: 2, name: 'Apples', description: 'Some description 2', category: 'Fruits', price: 0.89, unit: 'kg', quantity: 60, expiryDate: '2021-11-20', providerId: 1, providerName: 'Luca Bianchi', active: 1 },
+]
+
+describe('test GetAllProducts', () => {
+    test('No errors', async () => {
+        fetch.mockResponseOnce(JSON.stringify(products));
+        API.getAllProducts().then((res) => {
+            expect.assertions(22);
+            expect(res[0].id).toBe(1);
+            expect(res[0].name).toEqual('Carrots');
+            expect(res[0].description).toEqual('Some description 1');
+            expect(res[0].category).toEqual('Vegetables');
+            expect(res[0].price).toBe(1.23);
+            expect(res[0].unit).toEqual('kg');
+            expect(res[0].quantity).toBe(110);
+            expect(res[0].expiryDate).toEqual('2021-11-20');
+            expect(res[0].providerId).toBe(1);
+            expect(res[0].providerName).toEqual('Luca Bianchi');
+            expect(res[0].active).toBe(1);
+            expect(res[1].id).toBe(2);
+            expect(res[1].name).toEqual('Apples');
+            expect(res[1].description).toEqual('Some description 2');
+            expect(res[1].category).toEqual('Fruits');
+            expect(res[1].price).toBe(0.89);
+            expect(res[1].unit).toEqual('kg');
+            expect(res[1].quantity).toBe(60);
+            expect(res[1].expiryDate).toEqual('2021-11-20');
+            expect(res[1].providerId).toBe(1);
+            expect(res[1].providerName).toEqual('Luca Bianchi');
+            expect(res[1].active).toBe(1);
+        });
+    });
+    test('Error', async () => {
+        fetch.mockResponseOnce(JSON.stringify('API error'), { status: 500 });
+        API.getAllProducts().catch((res) => {
+            expect.assertions(2);
+            expect(res.status).toBe(500);
+            expect(res.errObj).toEqual('API error');
+        });
+    });
+});
+
+describe('test GetProductByID', () => {
+    test('No errors', async () => {
+        fetch.mockResponseOnce(JSON.stringify(products[0]));
+        API.getProductById(1).then((res) => {
+            expect.assertions(11);
+            expect(res.id).toBe(1);
+            expect(res.name).toEqual('Carrots');
+            expect(res.description).toEqual('Some description 1');
+            expect(res.category).toEqual('Vegetables');
+            expect(res.price).toBe(1.23);
+            expect(res.unit).toEqual('kg');
+            expect(res.quantity).toBe(110);
+            expect(res.expiryDate).toEqual('2021-11-20');
+            expect(res.providerId).toBe(1);
+            expect(res.providerName).toEqual('Luca Bianchi');
+            expect(res.active).toBe(1);
+        });
+    });
+    test('Error', async () => {
+        fetch.mockResponseOnce(JSON.stringify('API error'), { status: 500 });
+        API.getProductById(1).catch((res) => {
+            expect.assertions(2);
+            expect(res.status).toBe(500);
+            expect(res.errObj).toEqual('API error');
+        });
+    });
+});
+
+/*______________________________________________________________________*/
+/*                      CATEGORIES API TESTING                          */
+/*______________________________________________________________________*/
+
+const categories = [
+    { name: 'Fruits', active: 0 },
+    { name: 'Vegetables', active: 0 },
+    { name: 'Dairy', active: 0 },
+    { name: 'Butchery', active: 0 },
+]
+
+describe('test GetAllCategories', () => {
+    test('No errors', async () => {
+        fetch.mockResponseOnce(JSON.stringify(categories));
+        API.getAllCategories().then((res) => {
+            expect.assertions(8);
+            expect(res[0].name).toEqual('Fruits');
+            expect(res[0].active).toBe(0);
+            expect(res[1].name).toEqual('Vegetables');
+            expect(res[1].active).toBe(0);
+            expect(res[2].name).toEqual('Dairy');
+            expect(res[2].active).toBe(0);
+            expect(res[3].name).toEqual('Butchery');
+            expect(res[3].active).toBe(0);
+        });
+    });
+    test('Error', async () => {
+        fetch.mockResponseOnce(JSON.stringify('API error'), { status: 500 });
+        API.getAllCategories().catch((res) => {
+            expect.assertions(2);
+            expect(res.status).toBe(500);
+            expect(res.errObj).toEqual('API error');
+        });
+    });
+});
+
+/*______________________________________________________________________*/
+/*                      PROVIDERS API TESTING                           */
+/*______________________________________________________________________*/
+
+const providers = [
+    { id: 1, name: 'Luca Bianchi', description: 'Very good farmer', location: 'Torino' },
+    { id: 2, name: 'Luca Neri', description: 'Very bad farmer', location: 'Cuneo' }
+]
+
+describe('test GetAllProviders', () => {
+    test('No errors', async () => {
+        fetch.mockResponseOnce(JSON.stringify(providers));
+        API.getAllProviders().then((res) => {
+            expect.assertions(8);
+            expect(res[0].id).toBe(1);
+            expect(res[0].name).toEqual('Luca Bianchi');
+            expect(res[0].description).toEqual('Very good farmer');
+            expect(res[0].location).toEqual('Torino');
+            expect(res[1].id).toBe(2);
+            expect(res[1].name).toEqual('Luca Neri');
+            expect(res[1].description).toEqual('Very bad farmer');
+            expect(res[1].location).toEqual('Cuneo');
+        });
+    });
+    test('Error', async () => {
+        fetch.mockResponseOnce(JSON.stringify('API error'), { status: 500 });
+        API.getAllProviders().catch((res) => {
+            expect.assertions(2);
+            expect(res.status).toBe(500);
+            expect(res.errObj).toEqual('API error');
+        });
+    });
+});
+
+describe('test GetProviderByID', () => {
+    test('No errors', () => {
+        fetch.mockResponseOnce(JSON.stringify(providers[0]));
+        API.getProviderById(1).then((res) => {
+            expect.assertions(4);
+            expect(res.id).toBe(1);
+            expect(res.name).toEqual('Luca Bianchi');
+            expect(res.description).toEqual('Very good farmer');
+            expect(res.location).toEqual('Torino');
+        });
+    });
+    test('Error', async () => {
+        fetch.mockResponseOnce(JSON.stringify('API error'), { status: 500 });
+        API.getProviderById(1).catch((res) => {
+            expect.assertions(2);
+            expect(res.status).toBe(500);
+            expect(res.errObj).toEqual('API error');
+        });
+    });
+});
