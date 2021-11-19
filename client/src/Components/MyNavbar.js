48460f536bf2f7e31ce89b97d3b753bc2176fdc8
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-11-19 17:04:55 +0100

    Update MyNavbar.js

--- client/src/Components/MyNavbar.js
+++ client/src/Components/MyNavbar.js
@@ -5,24 +5,25 @@ const Navbar = function (props) {
 
         <nav className="navbar navbar-expand-lg menu-bar">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Solidarity Purchasing Group</a>
+                <a className="navbar-brand"  style={{'fontSize': 30}}href="#">Solidarity Purchasing Group</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{'fontSize': 30}}>
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="/home">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/client">Client</a>
+                            <a className="nav-link active" aria-current="page" href="/login">Client</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/employee">Staff</a>
+                            <a className="nav-link active" aria-current="page" href="/login">Staff</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="/farmer">Farmer</a>
                         </li>
+                       
                     </ul>
                     <form className="form-inline">
                         <select onChange={(event) => props.setTime((time) => ({ day: event.target.value, hour: time.hour }))} defaultValue={props.time.day} className="form-control-sm mr-2">
