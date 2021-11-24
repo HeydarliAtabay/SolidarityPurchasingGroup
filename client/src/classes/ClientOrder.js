96ddb9fa88759bb3299d61eef1faef01578fab8f
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-11-24 07:15:49 +0100

    Update ClientOrder.js

--- client/src/classes/ClientOrder.js
+++ client/src/classes/ClientOrder.js
@@ -1,8 +1,11 @@
 
-function clientOrders(order_id,client_id,product_name,state){
+
+function clientOrders(order_id,client_id,product_name,state,OrderPrice,id){
     this.order_id=order_id; 
     this.client_id=client_id; 
     this.product_name=product_name;
     this.state=state;
+    this.OrderPrice=OrderPrice;
+    this.id=id;
     }
-export  {clientOrders};
+export  {clientOrders};rders};
