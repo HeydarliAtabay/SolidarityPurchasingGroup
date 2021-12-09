5a7af32a84d3f732a70018513af0bea83a59f3c3
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-12-09 16:15:05 +0100

    added card for bookings

--- client/src/Components/FarmerArea.js
+++ client/src/Components/FarmerArea.js
@@ -152,6 +152,32 @@ function FarmerArea(props) {
             </div>
           </div>
         </div>
+        <div className="row w-100">
+          <div className="col-lg-12">
+            <div className="card m-3 w-100">
+              <div className="row no-gutters">
+                <div className="col-md-4 bg-secondary p-3 text-center">
+                  {walletIcon}
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">Bookings</h5>
+                    <p className="card-text">
+                      • See the list of all the booked orders
+                    </p>
+                    <div className="d-block text-end">
+                      <Link to="/see-bookings">
+                        <button className="btn btn-primary">
+                          See bookings
+                        </button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -196,5 +222,16 @@ const perparationIcon = (
     <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5v-7zm1.294 7.456A1.999 1.999 0 0 1 4.732 11h5.536a2.01 2.01 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456zM12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12v4zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
   </svg>
 );
-
+const walletIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="128"
+    height="128"
+    fill="currentColor"
+    className="bi bi-wallet"
+    viewBox="0 0 16 16"
+  >
+    <path d="M0 3a2 2 0 0 1 2-2h13.5a.5.5 0 0 1 0 1H15v2a1 1 0 0 1 1 1v8.5a1.5 1.5 0 0 1-1.5 1.5h-12A2.5 2.5 0 0 1 0 12.5V3zm1 1.732V12.5A1.5 1.5 0 0 0 2.5 14h12a.5.5 0 0 0 .5-.5V5H2a1.99 1.99 0 0 1-1-.268zM1 3a1 1 0 0 0 1 1h12V2H2a1 1 0 0 0-1 1z" />
+  </svg>
+);
 export default FarmerArea;
