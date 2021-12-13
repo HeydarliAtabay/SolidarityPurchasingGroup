bae86d7c8cfd1d60312694738d913b242b450158
Sara-y2 <79771350+Sara-y2@users.noreply.github.com>
2021-12-13 20:38:29 +0100

    Update ClientAlert.test.js

--- client/src/__tests__/ClientAlert.test.js
+++ client/src/__tests__/ClientAlert.test.js
@@ -1,12 +1,16 @@
 
+
 import React from 'react';
 import {  render } from '@testing-library/react';
 import ClientAlert from '../Components/ClientAlert'
-const testClients = [{ client_id: 2, budget:0, name: "Atabay", surname: "Heydarli", gender: 1, birthday: "1999-01-15", country: "Italy",
-region: "Piemonte",address: "Via Gambasca 19",city: "Torino"}];
+
+/*TEST DATA*/
+const testOrders = [{ order_id: 1, client_id: 2, product_name: "fennels",product_id:2,order_quantity:1,state:"booked",farmer_state:null,OrderPrice: 20, id:1,address:null,city:null,zipcode:0, Nation:null,date:null,time:null }];
+const testClients = [{ client_id: 2, budget:0, name: "Alan", surname: "Green", gender: 1, birthday: "1995-01-15", country: "Italy",region: "Piemonte",address: "Via Gardena 19",city: "Torino"}];
+const testUserid=2;
 
 test('Renders Correctly the client Reminder', () => { 
-const renderResult = render(<ClientAlert clients={testClients}clientid={2}/>);
+const renderResult = render(<ClientAlert clients={testClients} orders={testOrders} clientid={testUserid}/>);
 const AlertH = document.querySelector("[data-testid=alertH]");
 const Alert = document.querySelector("[data-testid=alert]");
 expect(AlertH.innerHTML).toBe("-ATTENTION-");
